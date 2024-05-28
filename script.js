2ae72cf7214db2882467764595a1bb528b93107c
SushmithaGopari <goparisushmitha@gmail.com>
2024-05-28 19:43:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const symbol = "!@#$%^&*()_+={}[]|\:;<>?/";
+const number ="0123456789";
+
+function createPassword () {
+    let password =""
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+}
+
+var firstWord = "Deepak";
+var secondWord = "Aman";
+
+isAnagram(firstWord, secondWord); // true
+
+function isAnagram(one, two) {
+  //Change both words to lowercase for case insensitivity..
+  var a = one.toLowerCase();
+  var b = two.toLowerCase();
+
+  // Sort the strings, then combine the array to a string. Examine the outcomes.
+  a = a.split("").sort().join("");
+  b = b.split("").sort().join("");
+
+  return a === b;
+}
+10
